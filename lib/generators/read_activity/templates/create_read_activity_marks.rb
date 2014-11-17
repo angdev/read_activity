49c0a239ae53175d708ee0bb7383c2652889810d
Hong ChulJu <ang0123dev@gmail.com>
2014-11-17 22:43:50 +0900

    added index -> reader, readable (#1)

--- lib/generators/read_activity/templates/create_read_activity_marks.rb
+++ lib/generators/read_activity/templates/create_read_activity_marks.rb
@@ -1,8 +1,8 @@
 class CreateReadActivityMarks < ActiveRecord::Migration
   def change
     create_table :read_activity_marks do |t|
-      t.references :reader, null: false
-      t.references :readable, polymorphic: true
+      t.references :reader, null: false, index: true
+      t.references :readable, polymorphic: true, index: true
       t.integer :mark, default: 0
 
       t.timestamps
